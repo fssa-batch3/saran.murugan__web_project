f573325c9ebfdfbd81ee029c520aaaef300a436b
freakysaran <saransaran32866@gmail.com>
2023-05-03 13:48:14 +0530

    finshed ticket booking page,showtime page

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -51,56 +51,32 @@ movie_lists.filter(function (movie){
       }
 }) ;
 
-const arrows = document.querySelectorAll(".arrow");
-const movieLists = document.querySelectorAll(".movie-list ");
-  
-
-arrows.forEach((arrow, i) => {
- let itemNumber = movieLists[i].querySelectorAll("img").length;
- let clickCounter = 0;
-arrow.addEventListener("click" , () => {
-      clickCounter++;
-      if(itemNumber - ( 4 + clickCounter)>= 0){
-          movieLists[i].style.transform = `translateX(${
-      movieLists[i].computedStyleMap().get("transform")[0].x.value
-      -300}px)`;
-      }else{
-          movieLists[i].style.transform = "translateX(0)";
-          clickCounter = 0;
-      }
-    
-});
-console.log(movieLists[i].querySelectorAll("img").length);
-});
-
-
-
 movie_lists.filter(function (movie){
-  if(movie.status && movie.language == "English"){
+  if(movie.status && movie.language == "English" ){
 
 
 
      let movie_list_items=document.createElement("div")
-          movie_list_items.setAttribute("class","movie-list-item2");
+          movie_list_items.setAttribute("class","movie-list-item");
 
      let   main_image = document.createElement("img");
-       main_image.setAttribute("class","movie-list-item-img2");
+       main_image.setAttribute("class","movie-list-item-img");
        main_image.setAttribute("src",movie.images.main_image);
        movie_list_items.append(main_image);
 
      let   title = document.createElement("span");
        title.innerText = movie.name.movie_name;
-       title.setAttribute("class","movie-list-item-title2");
+       title.setAttribute("class","movie-list-item-title");
        movie_list_items.append(title);
 
     let   description = document.createElement("p");
        description.innerText = movie.Description;
-       description.setAttribute("class","movie-list-item-desc2");
+       description.setAttribute("class","movie-list-item-desc");
        movie_list_items.append(description);
 
     let movie_button=document.createElement("button");
       //  movie_button.innerText="book now";
-       movie_button.setAttribute("class","movie-list-item-button2");
+       movie_button.setAttribute("class","movie-list-item-button");
        movie_list_items.append(movie_button);
 
 
@@ -109,77 +85,141 @@ movie_lists.filter(function (movie){
        a.innerText="book now";
        movie_button.append(a);
 
-       document.querySelector(".movie-list2").append(movie_list_items);
+       document.querySelector(".movie-list").append(movie_list_items);
     
 
       }
-      if(movie.status && movie.language == "Telugu"){
-        let movie_list_items=document.createElement("div")
-          movie_list_items.setAttribute("class","movie-list-item2");
+}) ;
 
-     let   main_image = document.createElement("img");
-       main_image.setAttribute("class","movie-list-item-img2");
-       main_image.setAttribute("src",movie.images.main_image);
-       movie_list_items.append(main_image);
+// const arrows = document.querySelectorAll(".arrow");
+// const movieLists = document.querySelectorAll(".movie-list ");
+  
 
-     let   title = document.createElement("span");
-       title.innerText = movie.name.movie_name;
-       title.setAttribute("class","movie-list-item-title2");
-       movie_list_items.append(title);
+// arrows.forEach((arrow, i) => {
+//  let itemNumber = movieLists[i].querySelectorAll("img").length;
+//  let clickCounter = 0;
+// arrow.addEventListener("click" , () => {
+//       clickCounter++;
+//       if(itemNumber - ( 4 + clickCounter)>= 0){
+//           movieLists[i].style.transform = `translateX(${
+//       movieLists[i].computedStyleMap().get("transform")[0].x.value
+//       -300}px)`;
+//       }else{
+//           movieLists[i].style.transform = "translateX(0)";
+//           clickCounter = 0;
+//       }
+    
+// });
+// console.log(movieLists[i].querySelectorAll("img").length);
+// });
 
-    let   description = document.createElement("p");
-       description.innerText = movie.Description;
-       description.setAttribute("class","movie-list-item-desc2");
-       movie_list_items.append(description);
-
-    let movie_button=document.createElement("button");
-      //  movie_button.innerText="book now";
-       movie_button.setAttribute("class","movie-list-item-button2");
-       movie_list_items.append(movie_button);
 
 
-   let a = document.createElement("a");
-       a.setAttribute("href", "./pages/movie_details.html?name=" +movie["name"]["movie_name"]);
-       a.innerText="book now";
-       movie_button.append(a);
+// // movie_lists.filter(function (movie){
+// //   if(movie.status && movie.language == "English"){
 
-       document.querySelector(".movie-list2").append(movie_list_items);
-      }
+
+
+// //      let movie_list_items=document.createElement("div")
+// //           movie_list_items.setAttribute("class","movie-list-item2");
+
+// //      let   main_image = document.createElement("img");
+// //        main_image.setAttribute("class","movie-list-item-img2");
+// //        main_image.setAttribute("src",movie.images.main_image);
+// //        movie_list_items.append(main_image);
+
+// //      let   title = document.createElement("span");
+// //        title.innerText = movie.name.movie_name;
+// //        title.setAttribute("class","movie-list-item-title2");
+// //        movie_list_items.append(title);
+
+// //     let   description = document.createElement("p");
+// //        description.innerText = movie.Description;
+// //        description.setAttribute("class","movie-list-item-desc2");
+// //        movie_list_items.append(description);
+
+// //     let movie_button=document.createElement("button");
+// //       //  movie_button.innerText="book now";
+// //        movie_button.setAttribute("class","movie-list-item-button2");
+// //        movie_list_items.append(movie_button);
+
+
+// //    let a = document.createElement("a");
+// //        a.setAttribute("href", "./pages/movie_details.html?name=" +movie["name"]["movie_name"]);
+// //        a.innerText="book now";
+// //        movie_button.append(a);
+
+// //        document.querySelector(".movie-list2").append(movie_list_items);
+    
+
+//       }
+//       if(movie.status && movie.language == "Telugu"){
+//         let movie_list_items=document.createElement("div")
+//           movie_list_items.setAttribute("class","movie-list-item2");
+
+//      let   main_image = document.createElement("img");
+//        main_image.setAttribute("class","movie-list-item-img2");
+//        main_image.setAttribute("src",movie.images.main_image);
+//        movie_list_items.append(main_image);
+
+//      let   title = document.createElement("span");
+//        title.innerText = movie.name.movie_name;
+//        title.setAttribute("class","movie-list-item-title2");
+//        movie_list_items.append(title);
+
+//     let   description = document.createElement("p");
+//        description.innerText = movie.Description;
+//        description.setAttribute("class","movie-list-item-desc2");
+//        movie_list_items.append(description);
+
+//     let movie_button=document.createElement("button");
+//       //  movie_button.innerText="book now";
+//        movie_button.setAttribute("class","movie-list-item-button2");
+//        movie_list_items.append(movie_button);
+
+
+//    let a = document.createElement("a");
+//        a.setAttribute("href", "./pages/movie_details.html?name=" +movie["name"]["movie_name"]);
+//        a.innerText="book now";
+//        movie_button.append(a);
+
+//        document.querySelector(".movie-list2").append(movie_list_items);
+//       }
 
  
-if(movie.status && movie.language == "Kanada"){
-    let movie_list_items=document.createElement("div")
-      movie_list_items.setAttribute("class","movie-list-item2");
+// if(movie.status && movie.language == "Kanada"){
+//     let movie_list_items=document.createElement("div")
+//       movie_list_items.setAttribute("class","movie-list-item2");
 
- let   main_image = document.createElement("img");
-   main_image.setAttribute("class","movie-list-item-img2");
-   main_image.setAttribute("src",movie.images.main_image);
-   movie_list_items.append(main_image);
+//  let   main_image = document.createElement("img");
+//    main_image.setAttribute("class","movie-list-item-img2");
+//    main_image.setAttribute("src",movie.images.main_image);
+//    movie_list_items.append(main_image);
 
- let   title = document.createElement("span");
-   title.innerText = movie.name.movie_name;
-   title.setAttribute("class","movie-list-item-title2");
-   movie_list_items.append(title);
+//  let   title = document.createElement("span");
+//    title.innerText = movie.name.movie_name;
+//    title.setAttribute("class","movie-list-item-title2");
+//    movie_list_items.append(title);
 
-let   description = document.createElement("p");
-   description.innerText = movie.Description;
-   description.setAttribute("class","movie-list-item-desc2");
-   movie_list_items.append(description);
+// let   description = document.createElement("p");
+//    description.innerText = movie.Description;
+//    description.setAttribute("class","movie-list-item-desc2");
+//    movie_list_items.append(description);
 
-let movie_button=document.createElement("button");
-  //  movie_button.innerText="book now";
-   movie_button.setAttribute("class","movie-list-item-button2");
-   movie_list_items.append(movie_button);
+// let movie_button=document.createElement("button");
+//   //  movie_button.innerText="book now";
+//    movie_button.setAttribute("class","movie-list-item-button2");
+//    movie_list_items.append(movie_button);
 
 
-let a = document.createElement("a");
-   a.setAttribute("href", "./pages/movie_details.html?name=" +movie["name"]["movie_name"]);
-   a.innerText="book now";
-   movie_button.append(a);
+// let a = document.createElement("a");
+//    a.setAttribute("href", "./pages/movie_details.html?name=" +movie["name"]["movie_name"]);
+//    a.innerText="book now";
+//    movie_button.append(a);
 
-   document.querySelector(".movie-list2").append(movie_list_items);
-  }
-});
+//    document.querySelector(".movie-list2").append(movie_list_items);
+//   }
+
 
 
 const arrows2 = document.querySelectorAll(".arrow2");
@@ -206,3 +246,12 @@ arrow.addEventListener("click" , () => {
 });
 console.log(movieLists2[i].querySelectorAll("img").length);
 });
+
+
+
+if(JSON.parse(localStorage.getItem("show_movie_details"))){
+
+}
+else{
+  
+}
